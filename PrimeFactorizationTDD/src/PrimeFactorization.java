8d4335cebe1ac179e06d41ddde46f44950299b68
budocf <budocf@.rose-hulman.edu>
2015-03-24 17:56:41 -0400

    test 6 refactor

--- PrimeFactorizationTDD/src/PrimeFactorization.java
+++ PrimeFactorizationTDD/src/PrimeFactorization.java
@@ -8,9 +8,8 @@ public class PrimeFactorization {
 
 	public static ArrayList<Integer> Factors(int i) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
-		while (i % 2 == 0) {
+		for (;i % 2 == 0;i/=2) {
 			factors.add(2);
-			i /= 2;
 		}
 		if (i > 1) {
 			factors.add(i);
