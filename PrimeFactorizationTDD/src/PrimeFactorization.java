ad37711463dd67534fe07dd938676caa98076065
budocf <budocf@.rose-hulman.edu>
2015-03-24 15:09:00 -0400

    Added first test case

---
+++ PrimeFactorizationTDD/src/PrimeFactorization.java
@@ -0,0 +1,11 @@
+import java.awt.List;
+
+
+public class PrimeFactorization {
+
+	public static List Factors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
--- PrimeFactorizationTDD/src/PrimeFactorizationUnitTest.java
+++ PrimeFactorizationTDD/src/PrimeFactorizationUnitTest.java
@@ -1,13 +1,15 @@
 import static org.junit.Assert.*;
 
+import java.awt.List;
+
 import org.junit.Test;
 
 
 public class PrimeFactorizationUnitTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOne() {
+		assertEquals(new List(), PrimeFactorization.Factors());
 	}
 
 }
