1a34f9a76b92e3ce6fc155713c02c36b87d225a1
budocf <budocf@.rose-hulman.edu>
2015-03-24 17:36:55 -0400

    test cases pass

--- PrimeFactorizationTDD/src/PrimeFactorization.java
+++ PrimeFactorizationTDD/src/PrimeFactorization.java
@@ -2,17 +2,22 @@ import java.awt.List;
 import java.util.ArrayList;
 import java.util.Collection;
 
-
 public class PrimeFactorization {
 
-	public static ArrayList<Integer> Factors()
-	{
+	public static ArrayList<Integer> Factors() {
 		return new ArrayList<Integer>();
 	}
+
 	public static ArrayList<Integer> Factors(int i) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
-		factors.add(i);
+		if (i % 2 == 0) {
+			factors.add(2);
+			i /= 2;
+		}
+		if (i > 1) {
+			factors.add(i);
+		}
 		return factors;
 	}
-	
+
 }
