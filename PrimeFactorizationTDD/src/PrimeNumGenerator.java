72fe22568ff5c5b52957409c454b6f645caa8d28
budocf <budocf@.rose-hulman.edu>
2015-03-27 23:27:02 -0400

    all tests passing

--- PrimeFactorizationTDD/src/PrimeNumGenerator.java
+++ PrimeFactorizationTDD/src/PrimeNumGenerator.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
 
-
-
-
 public class PrimeNumGenerator {
 	public Object PrimeNumGenerator(int i)
 	{
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		if(i==3)
+		for(int j = 0; j<i;j++)
 		{
-			list.add(2);
-		}
-		if(i==5)
-		{
-			list.add(2);
-			list.add(3);
-		}
-		if(i==7)
-		{
-			list.add(2);
-			list.add(3);
-			list.add(5);
+			if(checkPrimes(j))
+			{
+				list.add(j);
+			}
 		}
 		return list;
 	}
+	public boolean checkPrimes(int n)
+	{
+		if (n <= 3) {
+	        return n > 1;
+	    } else if (n % 2 == 0 || n % 3 == 0) {
+	        return false;
+	    } else {
+	        for (int i = 5; i * i <= n; i += 6) {
+	            if (n % i == 0 || n % (i + 2) == 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+	}
+	
 }
