f75e3e300e8db27d1e092474b7ce035c1ce2afaf
budocf <budocf@.rose-hulman.edu>
2015-03-27 23:16:56 -0400

    test 1 working

--- PrimeFactorizationTDD/src/PrimeNumGenerator.java
+++ PrimeFactorizationTDD/src/PrimeNumGenerator.java
@@ -7,6 +7,10 @@ public class PrimeNumGenerator {
 	public Object PrimeNumGenerator(int i)
 	{
 		ArrayList<Integer> list = new ArrayList<Integer>();
+		if(i==3)
+		{
+			list.add(2);
+		}
 		return list;
 	}
 }
